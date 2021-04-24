c0c52bc4b5e11ce5a8abe0d8bfc6c8c323d9409c
angelo milien <angelomilienjoseph@gmail.com>
2021-04-24 06:58:40 -0400

    commit tree a60b7533dc1501c35439ebe19263b4af4e65adfc

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,10 @@
 
 class Application
-    @@items = [Item.new("Apples",5.23), Item.new("Oranges",2.43)]
+    @@items = [Item.new("Apples",6), Item.new("Oranges",4)]
     def call(env)
       resp = Rack::Response.new
       req = Rack::Request.new(env)
-  
+    
       if req.path.match(/items/)
         item_name = req.path.split("/items/").last
         if item =@@items.find{|i| i.name == item_name}
@@ -19,5 +19,5 @@ class Application
       end
   
       resp.finish
-    end
+    end 
   end
